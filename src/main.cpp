df390d72d9d5af46419c65b8e7758978c7c255f7
Ulaş Sakın <mulassakin@gmail.com>
2025-08-31 21:36:36 +0300

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,14 @@ enum AppState {
     ABOUT
 };
 
+String notes[20];   // max 20 not
+int noteCount = 0;
+String currentNote = "";
+
 // Şu anki state'i tutan değişken
 AppState currentState = MENU;
+
+M5Canvas notecanvas(&M5Cardputer.Display);
 
 void drawMenu() {
     // Clear screen
@@ -45,10 +51,20 @@ void drawAbout() {
     // Not: ileride geri dönüş için bir tuş da ekleyeceğiz
 }
 
+void canvasSetup(){
+  notecanvas.createSprite(M5Cardputer.Display.width()-10, 
+                            M5Cardputer.Display.height()-20);
+
+  notecanvas.setFont(&fonts::Font0);
+  notecanvas.setTextSize(2);
+  notecanvas.setTextScroll(true);
+}
+
 void setup() {
     auto cfg = M5.config();
     M5Cardputer.begin(cfg, true);
     M5Cardputer.Display.setRotation(1);
+    canvasSetup();
 
     // Başlangıçta menüyü çiz
     drawMenu();
@@ -79,9 +95,15 @@ void loop() {
 
         case NEW_NOTE: {
             // Geçici mesaj
-            M5Cardputer.Display.fillScreen(BLACK);
-            M5Cardputer.Display.setCursor(10, 40);
-            M5Cardputer.Display.println("New Note screen (TODO)");
+            if (currentNote == "") {
+              // İlk girişte ekranı temizle
+              notecanvas.fillScreen(BLACK);
+              notecanvas.setCursor(5, 5);
+              notecanvas.print("New Note (press Enter to save): ");
+              notecanvas.pushSprite(0,0);
+              currentNote = "";
+              
+            }
             if (M5Cardputer.Keyboard.isChange() && M5Cardputer.Keyboard.isPressed()) {
                 auto keys = M5Cardputer.Keyboard.keysState();
                 for (auto k : keys.word) {
@@ -89,17 +111,35 @@ void loop() {
                       drawMenu();
                       currentState = MENU;
                     }
+
+                    else if (keys.enter){
+                      if (noteCount < 20){
+                        notes[noteCount++] = currentNote;
+                      }
+                      currentNote = "";
+                      drawMenu();
+                      currentState = MENU;
+                    }
+
+                    else{
+                      
+                      currentNote += k;
+                      notecanvas.print((char)k);
+                      notecanvas.pushSprite(0,0);
+                    }
                 }
             }
+
+            
             // ileride buraya input sistemi gelecek
             break;
         }
 
         case VIEW_NOTES: {
             // Geçici mesaj
-            M5Cardputer.Display.fillScreen(BLACK);
-            M5Cardputer.Display.setCursor(10, 40);
-            M5Cardputer.Display.println("View Notes screen (TODO)");
+            notecanvas.fillScreen(BLACK);
+            notecanvas.setCursor(10, 40);
+            notecanvas.print("View Notes screen (TODO)");
             if (M5Cardputer.Keyboard.isChange() && M5Cardputer.Keyboard.isPressed()) {
                 auto keys = M5Cardputer.Keyboard.keysState();
                 for (auto k : keys.word) {
